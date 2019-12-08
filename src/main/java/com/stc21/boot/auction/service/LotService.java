6ce377f50dbfd4072e3d1d6952646c53c9b65971
Pavel Efimov <gangplank009@gmail.com>
2019-12-08 13:57:08 +0300

    Добавлена крон задача на обновление записей в БД(LotPriceScheduler.java)

--- src/main/java/com/stc21/boot/auction/service/LotService.java
+++ src/main/java/com/stc21/boot/auction/service/LotService.java
@@ -1,4 +1,10 @@
 package com.stc21.boot.auction.service;
 
+import com.stc21.boot.auction.entity.Lot;
+
+import java.util.List;
+
 public interface LotService {
+    List<Lot> getAllLots();
+    void updateAllLots(List<Lot> lots);
 }
--- src/main/java/com/stc21/boot/auction/service/LotServiceImpl.java
+++ src/main/java/com/stc21/boot/auction/service/LotServiceImpl.java
@@ -1,8 +1,13 @@
 package com.stc21.boot.auction.service;
 
+import com.stc21.boot.auction.entity.Lot;
 import com.stc21.boot.auction.repository.LotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class LotServiceImpl implements LotService {
@@ -10,4 +15,24 @@ public class LotServiceImpl implements LotService {
     @Autowired
     private LotRepository lotRepository;
 
+    @Override
+    public List<Lot> getAllLots() {
+        return lotRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public void updateAllLots(List<Lot> lots) {
+        lots.forEach(lot -> {
+            lotRepository.updateCurrentPrice(calcCurrentPrice(lot), lot.getId());
+        });
+    }
+
+    private Double calcCurrentPrice(Lot lot) {
+        Random random = new Random();
+        Double max = lot.getMaxPrice();
+        Double min = lot.getMinPrice();
+        double randomValue = min + (max - min) * random.nextDouble();
+        return randomValue;
+    }
 }
